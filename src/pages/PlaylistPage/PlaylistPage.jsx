20f72c8e7ec3c84ac8df213a49cd3dc559cd6d1b
Gasouu <gaspar.rigot@etu.univ-poitiers.fr>
2025-11-24 17:25:28 +0100

    feat(playlist-detail): ✨ récupération et affichage de l'ID depuis l'URL via useParams

--- src/pages/PlaylistPage/PlaylistPage.jsx
+++ src/pages/PlaylistPage/PlaylistPage.jsx
@@ -1,3 +1,14 @@
+// src/pages/PlaylistPage/PlaylistPage.jsx
+
+import { useParams } from 'react-router-dom';
+
 export default function PlaylistPage() {
-    return <div>Playlist Page</div>;
+  const { id } = useParams();
+
+  return (
+    <div>
+      <h1>Playlist Detail Page</h1>
+      <p>Playlist ID: {id}</p>
+    </div>
+  );
 }
